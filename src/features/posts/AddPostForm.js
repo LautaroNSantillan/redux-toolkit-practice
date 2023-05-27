0df759a3f2d2ef56ea647f16de2c108178e688f2
SantillanLautaroNahuel <santillanlautaronahuel@gmail.com>
2023-05-27 17:38:39 -0300

    authors

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { nanoid } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
 
 import { postAdded } from "./postsSlice";
+import { selectAllUsers } from "../users/usersSlice";
 
 import React from 'react'
 
@@ -10,19 +10,31 @@ const AddPostForm = () => {
     const dispatch = useDispatch();
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
+    const [userId, setUserId] = useState('');
+
+    const users = useSelector(selectAllUsers);
 
     const onTitleChanged = e => setTitle(e.target.value);
     const onContentChanged = e => setContent(e.target.value);
+    const onAuthorChanged = e => setUserId(e.target.value);
 
     const onSavePost = () => {
         if(title && content){
             dispatch(
-                postAdded(title, content)
+                postAdded(title, content, userId)
             );
             setTitle('');
             setContent('');
         }
     }
+
+    const canSave=Boolean(title) && Boolean(content) && Boolean(userId);
+
+    const usersOptions = users.map(user =>(
+        <option key={user.id} value={user.id} >
+            {user.name}
+        </option>
+    ))
 
   return (
     <section>
